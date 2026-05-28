5c71ae4c68e63d21ad934341ec36051d6b4eedfa
faraditamas7 <faraditamas19@gmail.com>
2026-05-28 20:20:54 +0200

    finish

---
+++ src/server.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pino from 'pino-http';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(pino());
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
